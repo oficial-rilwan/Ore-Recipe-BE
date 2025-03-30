2e321c5e7c91dae2e245e8198a2143450dd832ab
oficial-rilwan <aribidesirilwan1477@gmail.com>
2025-03-30 08:16:53 +0100

    file structure updates

--- src/server.ts
+++ src/server.ts
@@ -18,11 +18,11 @@ const app = express();
 const PORT = process.env.PORT || 8080;
 
 app.set("view engine", "ejs");
-app.set("views", path.join(__dirname, "views"));
+app.set("views", path.join(__dirname, "../views"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(path.join(__dirname, "../public")));
 app.use(cookieParser());
 
 app.use("/", appRoutes);
