13e1551cf62dc6c9c7f8bd3c53cfc7b1cb130a7e
oficial-rilwan <aribidesirilwan1477@gmail.com>
2025-03-31 20:16:36 +0100

    feature updates

--- src/routes/app.routes.ts
+++ src/routes/app.routes.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import expressAsyncHandler from "express-async-handler";
 import appController from "../controllers/app.controller";
 import isAuth from "../middleware/jwt.middleware";
+import userController from "../controllers/user.controller";
 
 const appRoutes = Router();
 
@@ -11,5 +12,8 @@ appRoutes.get("/search", isAuth, expressAsyncHandler(appController.search));
 appRoutes.get("/register", isAuth, expressAsyncHandler(appController.register as any));
 appRoutes.get("/auth", isAuth, expressAsyncHandler(appController.auth as any));
 appRoutes.get("/", isAuth, expressAsyncHandler(appController.home));
+
+appRoutes.post("/auth", expressAsyncHandler(userController.auth as any));
+appRoutes.post("/register", expressAsyncHandler(userController.signup as any));
 
 export default appRoutes;
--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -5,8 +5,6 @@ import isAuth from "../middleware/jwt.middleware";
 
 const userRoutes = Router();
 
-userRoutes.post("/auth", expressAsyncHandler(userController.auth as any));
-userRoutes.post("/register", expressAsyncHandler(userController.signup as any));
 userRoutes.post("/signout", expressAsyncHandler(userController.signout as any));
 userRoutes.post("/deactivate", isAuth, expressAsyncHandler(userController.deactivate as any));
 
